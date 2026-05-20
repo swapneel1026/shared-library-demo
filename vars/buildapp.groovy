3aee648e1675c5754fa95fc56f2184632637f331
swapneel1026 <swapneelshubham44@gmail.com>
2026-05-21 00:40:32 +0530

    Remove unnecessary sleep commands from build, deploy, and test scripts

--- vars/buildapp.groovy
+++ vars/buildapp.groovy
@@ -1,6 +1,5 @@
 def call() {
     echo "Building the application..."
     sh "echo Maven Build Started"
-    sh "sleep 2"
     sh "echo Build Successful"
 }
--- vars/runtests.groovy
+++ vars/runtests.groovy
@@ -1,6 +1,5 @@
 def call() {
     echo "Running test cases..."
     sh "echo Executing Unit Tests"
-    sh "sleep 2"
     sh "echo All Tests Passed"
 }
